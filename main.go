a5cd142abd8ded926c50bae8d72187cce8528ab0
steeplejack <kcgori1982@gmail.com>
2020-04-04 11:06:09 +0100

    Add two inputs

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-import "syscall/js"
+import (
+	"strconv"
+	"syscall/js"
+)
 
 func add(this js.Value, i []js.Value) interface{} {
 	// js.Global().Set("output", js.ValueOf(i[0].Int()+i[1].Int()))
-	println(js.ValueOf(i[0].Int() + i[1].Int()).String())
-	return js.ValueOf(i[0].Int() + i[1].Int())
+	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+
+	int1, err := strconv.Atoi(value1)
+	if err != nil {
+		panic(err)
+	}
+
+	int2, err := strconv.Atoi(value2)
+	if err != nil {
+		panic(err)
+	}
+
+	println(int1 + int2)
+	return js.ValueOf(int1 + int2)
 }
 
 func subtract(this js.Value, i []js.Value) interface{} {
